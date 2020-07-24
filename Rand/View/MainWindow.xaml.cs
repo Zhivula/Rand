5eefb5301bb5aaa18296bd1ddeaa2a4a37c78bae
Zhivula <56290740+Zhivula@users.noreply.github.com>
2020-07-24 18:46:59 +0300

    Preparatory work. Loading all necessary libraries. MVVM pattern

---
+++ Rand/View/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Rand.ViewModel;
+using System.Windows;
+
+namespace Rand
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
