ba22704fde09e753da3bfb6ae972977cd7335320
Zhivula <56290740+Zhivula@users.noreply.github.com>
2020-07-25 21:55:12 +0300

    Works, but needs improvements

--- Rand/Model/MainWindowModel.cs
+++ Rand/Model/MainWindowModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rand.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,22 @@ namespace Rand.Model
 {
     class MainWindowModel
     {
+        private int start;
+        private int finish;
+        private int count;
+        public MainWindowModel(int start, int finish, int count)
+        {
+            this.start = start;
+            this.finish = finish;
+            this.count = count;
+        }
+        public List<int> GetList()
+        {
+            Random rand = new Random();
+            var list = new List<int>(count);
+            finish++;//finish+1 - это чтобы было включительное значение, по умолчанию не включительно 
+            for(var i = 0; i < count; i++) list.Add(rand.Next(start, finish));
+            return list;
+        }
     }
 }
--- Rand/ViewModel/MainWindowViewModel.cs
+++ Rand/ViewModel/MainWindowViewModel.cs
@@ -1,12 +1,77 @@
-﻿using System;
+﻿using Rand.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Rand.ViewModel
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : INotifyPropertyChanged
     {
+        private string start;
+        private string finish;
+        private string result;
+        private string count;
+
+        public string Start
+        {
+            get => start;
+            set
+            {
+                start = value;
+                OnPropertyChanged(nameof(Start));
+            }
+        }
+        public string Finish
+        {
+            get => finish;
+            set
+            {
+                finish = value;
+                OnPropertyChanged(nameof(Finish));
+            }
+        }
+        public string Result
+        {
+            get => result;
+            set
+            {
+                result = value;
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+        public string Count
+        {
+            get => count;
+            set
+            {
+                count = value;
+                OnPropertyChanged(nameof(Count));
+            }
+        }
+        public MainWindowViewModel()
+        {
+
+        }
+        /// <summary>
+        /// Начало генерации чисел 
+        /// </summary>
+        public ICommand GenerateNumbers => new DelegateCommand(o =>
+        {
+            MainWindowModel model = new MainWindowModel(int.Parse(start), int.Parse(finish), int.Parse(count));
+            string result = string.Empty;
+            foreach (var i in model.GetList()) result += i.ToString()+"   "; 
+            Result = result;
+        });
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
     }
 }
